eb7bb0fc2fb0181a6fd375b438c098bee5c4f83c
asasn <as_asn@qq.com>
2019-12-24 01:42:21 +0800

    修改了关于字符集的一些参数

--- run.c
+++ run.c
@@ -34,8 +34,9 @@ int main(int argc, char *argv[])
     {
         printf("文件基本名：%s\n", fileNameWithoutExt);
         printf("文件类型是：%s\n", fileExt);
-        printf("文件路径是：%s\n", fFilename);
-        printf("---------------------------------n");
+        printf("文件目录是：%s\n", fFilename);
+        printf("完整路径是：%s\n", argv[1]);
+        printf("---------------------------------\n");
         if (strcmp(fileExt, ".txt") == 0)
         {
             printf("现在打开的是.txt文件");
@@ -45,10 +46,8 @@ int main(int argc, char *argv[])
             system(argv[1]);
         }
         if (strcmp(fileExt, ".c") == 0)
-        {
+        {            
             strcat(comstr, "gcc");
-            strcat(comstr, " ");
-            strcat(comstr, "-fexec-charset=gbk");
             strcat(comstr, " ");
             strcat(comstr, argv[1]);
             strcat(comstr, " ");
@@ -58,14 +57,12 @@ int main(int argc, char *argv[])
             strcat(comstr, " ");
             strcat(comstr, "&&");
             strcat(comstr, " ");
-            strcat(comstr, fileNameWithoutExt);
+            strcat(comstr, fileNameWithoutExt);            
             system(comstr);
         }
         if (strcmp(fileExt, ".cpp") == 0)
         {
             strcat(comstr, "g++");
-            strcat(comstr, " ");
-            strcat(comstr, "-fexec-charset=gbk");
             strcat(comstr, " ");
             strcat(comstr, argv[1]);
             strcat(comstr, " ");
@@ -86,6 +83,7 @@ int main(int argc, char *argv[])
     }
 
     printf("\n---------------------------------\n");
+    printf("完整命令是：%s\n", comstr);
     system("pause");
     
     return 0;
