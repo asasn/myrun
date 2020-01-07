a565be3cfd346e5d2ceb0816c194703b0e5436ae
asasn <as_asn@qq.com>
2020-01-08 01:12:49 +0800

    更新

--- run.c
+++ run.c
@@ -8,12 +8,11 @@ int main(int argc, char *argv[])
     char dirve[9];
     char fileNameWithoutExt[99];
     char filePath[99];
-    char fFilename[128];
+    char fFilename[128] = {};
     char comstr[255];
     _splitpath(argv[1], dirve, filePath, fileNameWithoutExt, fileExt);
     strcat(fFilename, dirve);
     strcat(fFilename, filePath);
-
     if (*fileExt != 0)
     {
         /*
@@ -21,56 +20,67 @@ int main(int argc, char *argv[])
         printf("文件类型是：%s\n", fileExt);
         printf("文件目录是：%s\n", fFilename);
         */
-        printf("完整路径是：%s\n", argv[1]);
-        printf("---------------------------------\n");
+        printf("完整路径是：%s\n\n", argv[1]);
+        strcat(comstr, "cd");
+        strcat(comstr, " /d ");
+        strcat(comstr, "\"");
+        strcat(comstr, fFilename);
+        strcat(comstr, "\"");
+        printf("%s\n", comstr);
+        system(comstr); //执行cd /d filepath
+        memset(comstr, 0, sizeof(comstr));  //清空字符串
+        //*/        
         if (strcmp(fileExt, ".txt") == 0)
         {
             printf("现在打开的是.txt文件");
         }
+        if (strcmp(fileExt, ".bat") == 0 || strcmp(fileExt, ".cmd") == 0)
+        {
+            strcat(comstr, "\"");
+            strcat(comstr, argv[1]);
+            strcat(comstr, "\"");
+        }
         if (strcmp(fileExt, ".py") == 0)
         {
-            system(argv[1]);
+            strcat(comstr, "\"");
+            strcat(comstr, argv[1]);
+            strcat(comstr, "\"");
+        }
+        if (strcmp(fileExt, ".ui") == 0)
+        {
+            strcat(comstr, "pyuic5 \"");
+            strcat(comstr, argv[1]);
+            strcat(comstr, "\" -o ");
+            strcat(comstr, fileNameWithoutExt);
+            strcat(comstr, ".py");
         }
         if (strcmp(fileExt, ".c") == 0)
         {
-            strcat(comstr, "gcc");
-            strcat(comstr, " ");
+            strcat(comstr, "gcc -fexec-charset=gbk \"");
             strcat(comstr, argv[1]);
-            strcat(comstr, " ");
-            strcat(comstr, "-o");
-            strcat(comstr, " ");
+            strcat(comstr, "\" -o \"");
             strcat(comstr, fileNameWithoutExt);
-            strcat(comstr, " ");
-            strcat(comstr, "&&");
-            strcat(comstr, " ");
+            strcat(comstr, "\" && \"");
             strcat(comstr, fileNameWithoutExt);
-            system(comstr);
+            strcat(comstr, "\"");
         }
         if (strcmp(fileExt, ".cpp") == 0)
         {
-            strcat(comstr, "g++");
-            strcat(comstr, " ");
+            strcat(comstr, "g++ \"");
             strcat(comstr, argv[1]);
-            strcat(comstr, " ");
-            strcat(comstr, "-o");
-            strcat(comstr, " ");
+            strcat(comstr, "\" && ");
             strcat(comstr, fileNameWithoutExt);
-            strcat(comstr, " ");
-            strcat(comstr, "&&");
-            strcat(comstr, " ");
-            strcat(comstr, fileNameWithoutExt);
-            system(comstr);
         }
     }
     else
     {
-        printf("---------------------------------\n");
-        printf("未匹配的当前文件类型，请检查输入！");
-        strcpy(comstr, "无命令");
+        printf("未匹配的当前文件类型，请检查输入！\n");
+        strcpy(comstr, "chdir");
     }
-
-    printf("\n---------------------------------\n");
-    printf("传入的命令是：%s\n\n", comstr);
+    printf("%s\n", comstr);
+    printf("------------------------------------------------------------------\n");
+    system(comstr);
+    printf("------------------------------------------------------------------\n");
     system("pause");
 
     return 0;
